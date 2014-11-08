3ade8013149dfe4bfd2f9212481bbbfc0df919f6
Mark Keinhoerster <mark@Marks-MBP.fritz.box>
2014-11-08 23:48:39 +0100

    somewhat colorful, rotatable and playable

---
+++ app/src/main/java/com/pixels/blockies/app/game/figures/FigureO.java
@@ -0,0 +1,29 @@
+package com.pixels.blockies.app.game.figures;
+
+/**
+ * Created by keinmark on 30.04.14.
+ */
+public class FigureO extends AbstractFigure {
+
+    private int[][][] figures = new int[][][]{
+            {
+                    {1, 0},
+                    {1, 1},
+                    {0, 1}
+            },
+            {
+                    {0, 1, 1},
+                    {1, 1, 0}
+            }
+    };
+
+    @Override
+    protected int getFigureCount() {
+        return figures.length;
+    }
+
+    @Override
+    public int[][] get() {
+        return figures[getCurrentRotation()];
+    }
+}
