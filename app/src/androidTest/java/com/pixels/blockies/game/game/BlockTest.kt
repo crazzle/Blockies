f7de248c3bbc53f2eeddbb9dec8dcda194191de6
Robert Meissner <robert.meissner@codecentric.de>
2023-02-02 09:37:35 +0100

    First JUnit test running

---
+++ app/src/androidTest/java/com/pixels/blockies/game/game/BlockTest.kt
@@ -0,0 +1,11 @@
+package com.pixels.blockies.game.game
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class BlockTest {
+    @Test
+    fun testAdd() {
+        assertEquals(42, Integer.sum(19, 23))
+    }
+}
